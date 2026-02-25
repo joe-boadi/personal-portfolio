b029d5233ea06c3d22370bcabdc8825ef331f54d
joe-boadi <joeboadi1@gmail.com>
2026-02-25 23:58:46 +0000

    fix: update types and links

--- app/data.ts
+++ app/data.ts
@@ -2,7 +2,7 @@ type Project = {
   name: string
   description: string
   link: string
-  video: string
+  imageLink: string
   id: string
 }
 
@@ -32,17 +32,17 @@ export const PROJECTS: Project[] = [
     name: 'Random Password Generator',
     description:
       'Instantly generate secure, random passwords with customizable options.',
-    link: 'https://pro.motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+    link: 'https://password-generator-app-kohl-ten.vercel.app/',
+    imageLink:
+      'https://i.postimg.cc/MGLrbsdC/pasword-generator.png',
     id: 'project1',
   },
   {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
-    link: 'https://motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    name: 'Bluetooth Intrusion Detection',
+    description: 'A simulation to demonstrate the use of AI to detect intrusions.',
+    link: 'https://github.com/joe-boadi/bluetooth-intrusion-detection',
+    imageLink:
+      'https://i.postimg.cc/d0xWFHBp/AI-Intrusion-Detection.webp',
     id: 'project2',
   },
 ]
