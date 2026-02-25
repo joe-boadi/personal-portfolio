e95b1d56034dee48ad8fd4c7c462d3f48fc3c44b
joe-boadi <joeboadi1@gmail.com>
2026-02-25 23:25:55 +0000

    feat: setup and initial commit

---
+++ app/data.ts
@@ -0,0 +1,125 @@
+type Project = {
+  name: string
+  description: string
+  link: string
+  video: string
+  id: string
+}
+
+type WorkExperience = {
+  company: string
+  title: string
+  start: string
+  end: string
+  link: string
+  id: string
+}
+
+type BlogPost = {
+  title: string
+  description: string
+  link: string
+  uid: string
+}
+
+type SocialLink = {
+  label: string
+  link: string
+}
+
+export const PROJECTS: Project[] = [
+  {
+    name: 'Random Password Generator',
+    description:
+      'Instantly generate secure, random passwords with customizable options.',
+    link: 'https://pro.motion-primitives.com/',
+    video:
+      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+    id: 'project1',
+  },
+  {
+    name: 'Motion Primitives',
+    description: 'UI kit to make beautiful, animated interfaces.',
+    link: 'https://motion-primitives.com/',
+    video:
+      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    id: 'project2',
+  },
+]
+
+export const WORK_EXPERIENCE: WorkExperience[] = [
+  {
+    company: 'AmaliTech',
+    title: 'Frontend Engineer/AWS Cloud Practitioner',
+    start: '2024',
+    end: '2025',
+    link: 'amalitech.com',
+    id: 'work1',
+  },
+  {
+    company: 'Tech Prodigies',
+    title: 'Software Engineer/Research Information Lead',
+    start: '2021',
+    end: '2023',
+    link: 'https://ibelick.com',
+    id: 'work2',
+  },
+  {
+    company: 'Freelance',
+    title: 'Front-end Developer',
+    start: '2021',
+    end: 'Present',
+    link: 'https://ibelick.com',
+    id: 'work3',
+  },
+]
+
+export const BLOG_POSTS: BlogPost[] = [
+  {
+    title: 'Exploring the Intersection of Design, AI, and Design Engineering',
+    description: 'How AI is changing the way we design',
+    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    uid: 'blog-1',
+  },
+  {
+    title: 'Why I left my job to start my own company',
+    description:
+      'A deep dive into my decision to leave my job and start my own company',
+    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    uid: 'blog-2',
+  },
+  {
+    title: 'What I learned from my first year of freelancing',
+    description:
+      'A look back at my first year of freelancing and what I learned',
+    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    uid: 'blog-3',
+  },
+  {
+    title: 'How to Export Metadata from MDX for Next.js SEO',
+    description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
+    link: '/blog/example-mdx-metadata',
+    uid: 'blog-4',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    label: 'Github',
+    link: 'https://github.com/joe-boadi',
+  },
+  {
+    label: 'Twitter',
+    link: 'https://twitter.com/joeboadi_Dev',
+  },
+  {
+    label: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/kwadjo-boadi-mantey-43b670271',
+  },
+  {
+    label: 'Instagram',
+    link: 'https://www.instagram.com/boadijoe',
+  },
+]
+
+export const EMAIL = 'joeboadi1@gmail.com'
